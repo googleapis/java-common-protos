f9f1854d121870c2496704659f4a137ddf6e93dc
Neenu1995 <neenushaji@google.com>
2021-08-26 18:57:35 -0400

    chore: remove BigqueryAuditMetadata

--- synth.py
+++ synth.py
@@ -96,6 +96,11 @@ build_grpc("//google/longrunning:longrunning_java_grpc")
 java.format_code("proto-google-common-protos/src")
 java.format_code("grpc-google-common-protos/src")
 
+os.remove("proto-google-common-protos/src/main/java/com/google/cloud/audit/BigQueryAuditMetadata.java")
+os.remove("proto-google-common-protos/src/main/java/com/google/cloud/audit/BigQueryAuditMetadataOrBuilder.java")
+os.remove("proto-google-common-protos/src/main/java/com/google/cloud/audit/BigQueryAuditMetadataProto.java")
+os.remove("proto-google-common-protos/src/main/proto/google/cloud/audit/bigquery_audit_metadata.proto")
+
 java.common_templates(excludes=[
   'README.md',
   'samples/*',
